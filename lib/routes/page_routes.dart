5e5c7ece6fabdad7d4631c28789c80ba01982bfa
Shahab Alam <iamShahabAlam@gmail.com>
2024-04-25 23:34:01 +0500

    routes updated

--- lib/routes/page_routes.dart
+++ lib/routes/page_routes.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:the_fire/presentation/auth/login/login_screen.dart';
+import 'package:the_fire/presentation/auth/register/register_screen.dart';
 import 'package:the_fire/presentation/auth/splash/splash_screen.dart';
 import 'package:the_fire/routes/page_names.dart';
 
 class PageRoutes {
   static final Map<String, WidgetBuilder> routes = {
     PageNames.splashScreen: (_) => const SplashScreen(),
+    PageNames.registerScreen: (_) => const RegisterScreen(),
+    PageNames.loginScreen: (_) => const LoginScreen(),
   };
 }
