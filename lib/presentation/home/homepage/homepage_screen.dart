7315cbbcc767b0dcbc019e59fc87b8a1e57cd8ac
Shahab Alam <iamShahabAlam@gmail.com>
2024-05-11 22:45:35 +0500

    get data update

--- lib/presentation/home/homepage/homepage_screen.dart
+++ lib/presentation/home/homepage/homepage_screen.dart
@@ -1,6 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:the_fire/app/utils/custom_functions/app_alerts.dart';
 import 'package:the_fire/app/utils/custom_widgets/common_text.dart';
 import 'package:the_fire/routes/page_names.dart';
@@ -16,6 +16,16 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  List<Map<String, dynamic>> productsList = [];
+  Map<String, dynamic> product = {};
+
+  @override
+  void initState() {
+    getProductsCollection();
+    getProductsDocument('0');
+    super.initState();
+  }
 
   onPressLogout() {
     _auth.signOut().then((value) {
@@ -26,13 +36,36 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+// To get the Entire Collection ---------------
+  getProductsCollection() async {
+    QuerySnapshot resp = await db.collection('products').get();
+    // .map method (preferred)
+    productsList = resp.docs.map((doc) {
+      return doc.data() as Map<String, dynamic>;
+    }).toList();
+
+    // .forEach method
+    /*
+    resp.docs.forEach((doc) {
+      var data = doc.data() as Map<String, dynamic>;
+      productsList.add(data);
+    });
+  */
+  }
+
+// get Individual Document --------------------
+  getProductsDocument(String docName) async {
+    DocumentSnapshot resp = await db.collection('products').doc(docName).get();
+    product = resp.data() as Map<String, dynamic>;
+  }
+
   @override
   Widget build(BuildContext context) {
     print(widget.profile);
     return Scaffold(
       appBar: AppBar(
         title: const CommonText(text: 'Home Page'),
-        actions: [IconButton(onPressed: () => onPressLogout(), icon: Icon(Icons.power_settings_new))],
+        actions: [IconButton(onPressed: () => onPressLogout(), icon: const Icon(Icons.power_settings_new))],
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
